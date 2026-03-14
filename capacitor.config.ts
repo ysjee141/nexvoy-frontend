a5a81efb2e6badd98785100698fabe2137925c1f
햅(지윤성)_엔터플랫폼개발 <ysjee141@nol-universe.com>
2026-03-14 20:02:37 +0900

    feat: apply branding assets (icons, splash) and rename app to Next Voyage

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,11 +2,19 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'xyz.nexvoy.app',
-  appName: 'NexVoy',
+  appName: 'Next Voyage',
   webDir: 'out',
   plugins: {
     CapacitorHttp: {
       enabled: true,
+    },
+    SplashScreen: {
+      launchShowDuration: 2000,
+      launchAutoHide: true,
+      backgroundColor: "#ffffffff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
     },
   },
   server: {
